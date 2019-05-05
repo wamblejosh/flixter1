16cbeea56a460d19a123a4fab5ebb6b7b7482160
wamblejosh <wamblejosh@gmail.com>
2019-05-05 18:26:20 +0000

    Student Enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
 	belongs_to :user
 	has_many :sections
 	has_many :images
+	has_many :enrollments
 	mount_uploader :image, ImageUploader 
 
 	validates :title, presence: true 
