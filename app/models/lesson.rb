bf3fb2b858dede31150b48915b40a9ca0dafa868
wamblejosh <wamblejosh@gmail.com>
2019-05-11 18:56:49 +0000

    Next and Previous button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,21 @@ class Lesson < ApplicationRecord
 
 	include RankedModel 
 	ranks :row_order, with_same: :section_id
+
+	def next_lesson
+		lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+		if lesson.blank? && section.next_section
+			return section.next_section.lessons.rank(:row_order).first
+		end
+		  return lesson
+	end
+
+
+	def prev_lesson
+		lesson = section.lessons.where("row_order < ?", self.row_order).rank(:row_order).last
+		if lesson.blank? && section.prev_section
+			return section.prev_section.lessons.rank(:row_order).last
+		end
+		  return lesson
+	end
 end
